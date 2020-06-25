f29ef9102a27a538e34cf98bb02fad9c57f534b6
dmitriy-chur <1236547890>
2020-06-25 21:19:47 +0300

    Initial commit

---
+++ src/main/java/com/custom/project/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.custom.project.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
+}
