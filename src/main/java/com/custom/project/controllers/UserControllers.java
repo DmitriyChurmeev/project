064428870ed5b7cbb8cd8d4b3a71bba56ab29cd5
dmitriy-chur <1236547890>
2020-07-06 00:01:39 +0300

    add annotation

--- src/main/java/com/custom/project/controllers/UserControllers.java
+++ src/main/java/com/custom/project/controllers/UserControllers.java
@@ -3,6 +3,7 @@ package com.custom.project.controllers;
 import com.custom.project.model.entities.User;
 import com.custom.project.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class UserControllers {
 
     private final UserRepository userRepository;
 
+    @GetMapping
     public List<User> getUsers(){
         return userRepository.findAll();
     }
