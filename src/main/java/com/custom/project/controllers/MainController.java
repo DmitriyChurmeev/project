c46400984e45b3a02bf908f94643fb941826f35a
dmitriy-chur <1236547890>
2020-07-08 22:57:07 +0300

    add property from config server

--- src/main/java/com/custom/project/controllers/MainController.java
+++ src/main/java/com/custom/project/controllers/MainController.java
@@ -1,13 +1,19 @@
 package com.custom.project.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class MainController {
 
+    @Value("${project.name}")
+    private String projectName;
+
     @GetMapping("test")
     public String test() {
-        return "test";
+        return projectName;
     }
 }
