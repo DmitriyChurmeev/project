f1a3a55e49b81fd985f4bc49e1a56f5ed4f3e873
dmitriy-chur <1236547890>
2020-09-18 00:41:40 +0300

    add test controller

--- src/main/java/com/custom/project/controllers/HomeController.java
+++ src/main/java/com/custom/project/controllers/HomeController.java
@@ -3,6 +3,8 @@ package com.custom.project.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
 @Slf4j
 @RestController
 public class HomeController {
@@ -12,12 +14,10 @@ public class HomeController {
     public String index(@RequestBody TestRequest o, @PathVariable("formId") Object formId) {
         log.error("request from yandex form");
         log.error("!!!!");
-        String result = (String) o.getParams().get("result");
-        log.error(result);
-        log.error(o.getParams().get("result").getClass().getSimpleName());
-       // log.error(result.get("ФИО").toString());
-       // log.error(result.get("Комментарий").toString());
+        log.error(o.getParams().toString());
+
 
         return "index";
     }
+
 }
