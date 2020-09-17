867a3963cd344e32739407c5ca661219f8a0879e
dmitriy-chur <1236547890>
2020-09-17 15:05:10 +0300

    add test controller

--- src/main/java/com/custom/project/controllers/HomeController.java
+++ src/main/java/com/custom/project/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.custom.project.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,8 +14,9 @@ public class HomeController {
 
     @PostMapping
     @RequestMapping(value = "/test/{formId}")
-    public String index(@RequestBody Object o, @PathVariable("formId") String formId) {
+    public String index(@RequestBody Object o, @PathVariable("formId") RequestEntity formId) {
         log.error("request from yandex form");
+        log.error(formId.getBody().toString());
         Map map = new ObjectMapper().convertValue(o, Map.class);
         log.error(map.toString());
         log.error(o.toString());
