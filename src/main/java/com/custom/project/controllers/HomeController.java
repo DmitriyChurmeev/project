a0d72d195f58cb2be52dc04f540d00868d2bd1be
dmitriy-chur <1236547890>
2020-09-17 16:27:37 +0300

    add test controller

--- src/main/java/com/custom/project/controllers/HomeController.java
+++ src/main/java/com/custom/project/controllers/HomeController.java
@@ -1,14 +1,7 @@
 package com.custom.project.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -19,9 +12,11 @@ public class HomeController {
     public String index(@RequestBody TestRequest o, @PathVariable("formId") Object formId) {
         log.error("request from yandex form");
         log.error("!!!!");
-        LinkedHashMap result = (LinkedHashMap) o.getParams().get("result");
-        log.error(result.get("ФИО").toString());
-        log.error(result.get("Комментарий").toString());
+        String result = (String) o.getParams().get("result");
+        log.error(result);
+        log.error(o.getParams().get("result").getClass().getSimpleName());
+       // log.error(result.get("ФИО").toString());
+       // log.error(result.get("Комментарий").toString());
 
         return "index";
     }
